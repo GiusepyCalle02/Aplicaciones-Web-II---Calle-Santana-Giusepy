2f63ff503a525d28d4a939537bab2ed3b69ac000
GiusepyCalle02 <e1315207892@live.uleam.edu.ec>
2022-10-07 00:29:39 -0500

    funcion de búsqueda con la estructura Async Await

---
+++ 1er-Parcial/2da-Práctica/estructura2.js
@@ -0,0 +1,41 @@
+import {aspirantes, inscripciones} from './datos.js';
+
+
+async function findAspiranteForId(id){
+    const aspirante = aspirantes.find((aspirante)=> aspirante.id===id );
+    if (!aspirante)
+    {
+        const error =  new Error();
+        error.message="Aspirante no encontrado";
+        throw error;
+    }
+    return aspirante;
+
+}
+
+async function findInscripcionForId(id){
+    const inscripcion =  inscripciones.find((inscripcion)=>{
+        return inscripcion.id===id;
+    });
+    if (!inscripcion)
+    {
+        const error = new Error();
+        error.message="No encontramos la inscripción";
+        throw error;
+    }
+    return inscripcion;
+}
+(async ()=>{
+    try
+    {
+        const aspirante  =   await findAspiranteForId(44);
+        const inscripcion =   await  findInscripcionForId(aspirante.id);
+        aspirante.inscripcion = inscripcion;
+        console.log(aspirante)
+    }
+    catch (err)
+    {
+        console.log(err.message)
+    }
+}
+)();
